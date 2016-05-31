aacd23b5730de93696eba8962a5296d4aca02b6f
blseich <blseich@gmail.com>
2016-05-31 15:58:10 -0400

    gulp runs code coverage tool, fails if coverage drops below 90%

--- karma.conf.js
+++ karma.conf.js
@@ -36,13 +36,26 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'js/**/*.js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    reporters: ['mocha', 'coverage'],
+
+    coverageReporter: {
+        type: 'text',
+        check: {
+            global: {
+                statements: 90,
+                lines: 90,
+                functions: 90,
+                branches: 90
+            }
+        }
+    },
 
 
     // web server port
